e6379498126677e540ba2a44a66cabac3ff98850
MariellaMartens <mariella.martens@students.hs-mainz.de>
2022-09-01 09:30:07 +0200

    Version mit der Überprüfung, ob es sich um eine IP-Adresse handelt

--- Program.cs
+++ Program.cs
@@ -7,19 +7,32 @@ namespace P2PVersuch01
     {
         static void Main(string[] args)
         {
+            Start:
             Thread thread1 = new Thread(threadAufgabe);
             thread1.Start();
             TcpSender sender = new TcpSender();
             Console.WriteLine("gib eine IP-Adresse ein:");
             string ipadresse = Console.ReadLine();
+            bool IsIP(string ipadresse)
+            {
+                return System.Text.RegularExpressions.Regex.IsMatch(ipadresse, @"\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$\b");
+            }
             //string ipadresse = System.Net.IPAddress.Loopback.ToString(); //statt Loopback die IpAdresse des anderen Laptops (beide gleichzeitig das Programm laufen lassen)
             //sender.senden(ipadresse, "hallo", 13000);
-            while (true)
+            while (!IsIP(ipadresse))
             {
-                Console.WriteLine("gib deine Nachricht ein:");
-                string nachricht = Console.ReadLine();
-                sender.senden(ipadresse, nachricht, 13000);
+                Console.WriteLine("gib eine IP-Adresse ein:");
+                ipadresse = Console.ReadLine();
+            }
+            if (IsIP(ipadresse))
+            {
+                while (true)
+                {
+                    Console.WriteLine("gib deine Nachricht ein:");
+                    string nachricht = Console.ReadLine();
+                    sender.senden(ipadresse, nachricht, 13000);
 
+                }
             }
         }
         public static void threadAufgabe()
